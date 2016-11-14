fa48c15d7f1efda87568b3c6c066c7ee576a8891
Aleksandr <zadov112@ya.ru>
2016-11-14 22:03:19 +0300

    map_hide

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 var search = document.querySelector(".search_btn");
 var form = document.querySelector(".hotel_search_form");
+var map_hide = document.querySelector(".map_hide");
 
 search.addEventListener("click", function(event) {
 	event.preventDefault();
 	form.classList.toggle("hotel_search_form_hide");
+	map_hide.classList.toggle("map_hide_disable");
 });
