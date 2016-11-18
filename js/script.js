500f7fed8e9d70cd96cc9a7190b2faf00668aa06
Aleksandr <zadov112@ya.ru>
2016-11-18 14:04:54 +0300

    js, prefix, inline-block

--- js/script.js
+++ js/script.js
@@ -15,13 +15,33 @@ var search = document.querySelector(".search_btn");
 var form = document.querySelector(".hotel_search_form");
 var map_hide = document.querySelector(".map_hide");
 
-// если включен js, то добавляем классы, чтобы спрятать форму по умолчанию
+var date_in = document.querySelector("[name=date_in]");
+var date_out = document.querySelector("[name=date_out]");
+var adults = document.querySelector("[name=adults]");
+
+var storage = localStorage.getItem("adults");
 
 if (form) {
+	// заполняем колличество взрослых, если уже была отправка формы
+	if (storage) {
+		adults.value = storage;
+	};
+	// если включен js, то добавляем классы, чтобы спрятать форму по умолчанию
 	form.classList.add("form_animate");
 	map_hide.classList.add("hide_block");
+	// проверяем на наличие данных
+	form.addEventListener("submit", function(event) {
+		if (!date_in.value || !date_out.value || !adults.value) {
+			event.preventDefault();
+			console.log("Нужно ввести логин и пароль"); 
+			alert ("Введите пожалуйста дату въезда, дату выезда и колличество взрослых");
+		} else {
+			localStorage.setItem("adults", adults.value);
+		}
+	});
 };
 
+// показываем и скрываем форму, если форма открыта, то работать с картой не возможно
 if (search) {
 	search.addEventListener("click", function(event) {
 		event.preventDefault();
@@ -29,16 +49,6 @@ if (search) {
 		map_hide.classList.toggle("hide_block");
 });
 };
-
-// проверяем на наличие данных
-
-var date_in = document.querySelector("[name=date_in]");
-var date_out = document.querySelector("[name=date_out]");
-var adults = document.querySelector("[name=adults]");
-
-
-
-
 
 // отключаем переход по ссылке, если включен javascript
 
